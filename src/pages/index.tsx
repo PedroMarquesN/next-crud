aa809daf02fdab81d1ad1082ab97635b52b2f2ea
Pedro Marques <pedroleagueartmarques@gmail.com>
2024-03-07 11:06:50 -0300

    Feat: Component Tabela , and Class Cliente

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,20 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Layout from "@/components/Layout";
+import Tabela from "@/components/Tabela";
+import Cliente from "../core/Cliente";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+
+    const clientes = [
+        new Cliente('Pedro', 18 , "32"),
+        new Cliente('Joao', 19, "34"),
+        new Cliente('Carlos', 28, "35"),
+
+    ]
   return (
       <div className={`
       bg-gradient-to-r from-blue-500 to-purple-500 
@@ -12,7 +22,7 @@ export default function Home() {
       text-white
       `}>
           <Layout titulo="Cadastro Simples">
-              <span>Conteudo</span>
+              <Tabela clientes={clientes}/>
           </Layout>
       </div>
 
